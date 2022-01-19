1b4fa6acd715ee14d6cde407c7cfce9c052322d3
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-19 07:10:35 +0700

    feat: add commit lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // TODO: Add nav & footer & SEO
   return <Component {...pageProps} />;
 }
 
