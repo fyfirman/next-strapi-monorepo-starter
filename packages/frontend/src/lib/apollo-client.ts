27c3e12b97bc42c2ed44193c364faedef5c6ef48
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-21 02:26:31 +0700

    fix: disable caching because graphql always caching

--- packages/frontend/src/lib/apollo-client.ts
+++ packages/frontend/src/lib/apollo-client.ts
@@ -5,6 +5,12 @@ const apolloClient = new ApolloClient({
   uri: env.graphqlUrl,
   cache: new InMemoryCache(),
   ssrMode: typeof window === "undefined",
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+  },
 });
 
 export default apolloClient;
