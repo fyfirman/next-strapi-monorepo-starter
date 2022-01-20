19d5e780c35421cba9659248e062546045da04a7
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-20 22:54:26 +0700

    chore: change name to frontend & web

---
+++ packages/frontend/next-seo.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://next-strapi.fyfirman.com/",
+    site_name: "Next Strapi Monorepo Starter",
+  },
+  title: "Next Strapi Monorepo Starter",
+};
