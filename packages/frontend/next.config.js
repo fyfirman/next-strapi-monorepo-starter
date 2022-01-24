b6fef4c239d32231af019924194b49d1675fc931
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-25 00:55:55 +0700

    fix: disabled eslint

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Notes: Disabled. Using eslint command on root of monorepo folder. 
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
