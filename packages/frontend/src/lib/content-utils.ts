2fa0578e9e43306dcb080e595b77b2184495320d
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-25 00:04:49 +0700

    chore: refactor content utils

--- packages/frontend/src/lib/content-utils.ts
+++ packages/frontend/src/lib/content-utils.ts
@@ -1,12 +1,14 @@
-import { ContentEntityResponseCollection, Maybe } from "~/@types/graphql";
+import { ContentEntityResponseCollection } from "~/@types/graphql";
 
 export type StringHashMap = { [key: string]: string };
 
-export function transformGraphQLResponseToObject(data: Maybe<ContentEntityResponseCollection>): StringHashMap {
+export function transformGraphQLResponseToObject(data: ContentEntityResponseCollection): StringHashMap {
   const obj = {};
 
-  data?.data.forEach((val) => {
-    Object.assign(obj, { [val.attributes?.key ?? ""]: val.attributes?.value });
+  data.data.forEach((val) => {
+    if (val.attributes?.key) {
+      Object.assign(obj, { [val.attributes.key]: val.attributes.value });
+    }
   });
 
   return obj;
